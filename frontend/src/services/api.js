4765e8b70a1833fe7e15054f1b1673a3b629a77a
kusum jha <jebracrossing123@gmail.com>
2026-05-30 23:26:34 +0530

    Update frontend API URL for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://wavenet-assignment-iov9.onrender.com/api"
 });
 
 export default API;
